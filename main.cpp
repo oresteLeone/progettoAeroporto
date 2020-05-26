6316ec6cba3e8f38124fe6e0709e2d98f1c1eefc
oresteLeone <64261072+oresteLeone@users.noreply.github.com>
2020-05-26 19:39:00 +0200

    main add

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "headers/definers.h"
+#include "headers/graphlib.h"
+#include "headers/utentilib.h"
+
+int main() {
+
+	printf("\nHello World\n");
+
+	return 0;
+
+}
+
