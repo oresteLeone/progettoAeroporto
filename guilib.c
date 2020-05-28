a59b6d225604e5da9b40afa076863a2ba06da6d2
D4ve95 <Davide_Sava_1995@outlook.it>
2020-05-28 16:51:26 +0200

    Modifica - Registrazione Modifica - Interfaccia iniziale Aggiunta - Login Aggiunta - Menù Admin Aggiunta - Menù Utente

--- guilib.c
+++ guilib.c
@@ -42,3 +42,81 @@ char* getString() {
     }
     return string;
 }
+
+//menù dell'admin
+void menùAdmin() {
+    char richiesta;
+    int quit = 0;
+
+    printf("\nMenu' Admin:\n");
+
+    while (1)
+    {
+        printf("\nInserisca '1' per aggiungere una destinazione\nInserisca '2' per aggiungere una tratta\n");
+        printf("Inserisca '3' per rimuovere una destinazione\nInserisca '4' per rimuovere una tratta\nInserisca '0' per il Logout\n");
+
+        richiesta = catchRequest();
+
+        switch (richiesta)
+        {
+        case '1':
+            printf("\nAggiunta di una destinazione:\n");
+            break;
+        case '2':
+            printf("\nAggiunta di una tratta:\n");
+            break;
+        case '3':
+            printf("\nRimozione di una destinazione:\n");
+            break;
+        case '4':
+            printf("\nRimozione di una tratta\n");
+            break;
+        case '0':
+            printf("\nLogout in corso...\n");
+            quit = 1;
+            break;
+        default:
+            printf("\nRichiesta non valida\n");
+            break;
+        }
+
+        if (quit == 1)
+            break;
+    }
+}
+
+//menù dell'utente
+void menùUtente() {
+    char richiesta;
+    int quit = 0;
+
+    printf("\nMenu' Utente:\n");
+
+    while (1)
+    {
+        printf("\nInserisca '1' per visualizzare le sue prenotazioni attive\nInserisca '2' per effettuare una prenotazione\n");
+        printf("Inserisca '0' per il Logout\n");
+
+        richiesta = catchRequest();
+
+        switch (richiesta)
+        {
+        case '1':
+            printf("\nPrenotazioni attive:\n");
+            break;
+        case '2':
+            printf("\nPrenotazione:\n");
+            break;
+        case '0':
+            printf("\nLogout in corso...\n");
+            quit = 1;
+            break;
+        default:
+            printf("\nRichiesta non valida\n");
+            break;
+        }
+
+        if (quit == 1)
+            break;
+    }
+}
--- main.c
+++ main.c
@@ -7,49 +7,69 @@
 #include "guilib.h"
 
 int main() {
-	char string[maxstring];
+	char nomeUtente[maxstring], passw[maxstring];
 	char richiesta;
-	int quit = 0;
-	char* user, passwd; 
-	int flag = 0;
-	
-	do {
-			printf("\nInserisca '1' per il Login\nInserisca '2' per Registrarsi\nInserisca '0' per Chiudere\n ");
-			richiesta = catchRequest();
-			user = (char*)malloc(sizeof(char) * 20);
-			passwd = (char*)malloc(sizeof(char) * 20);
+	int quit = 0, flag = 0;
+	Utente* radUtente = NULL;
 
-			switch (richiesta) {
-				case '1':
-					printf("\nLogin...\n");
-					printf("inserisci nome utente: ");
-					strcpy(string, getString());
-					printf("\nStringa inserita: %s lunghezza:%d ", string, strlen(string));
-					break;
-				case '2':
-					do {
-						printf("Salve, per registrarsi inserisca un username:\n");
-						scanf("%s", user);
-						flag = ricercaUtente(radUtente, user);
-						if (flag == 1)
-							printf("Nome utente non disponibile. Riprovi:\n");
+	do 
+	{
+		printf("\nInserisca '1' per il Login\nInserisca '2' per Registrarsi\nInserisca '0' per Chiudere\n ");
+		richiesta = catchRequest();
+
+		switch (richiesta) 
+		{
+			case '1':
+				printf("\nLogin:\n");
+
+				printf("\nNome utente: ");
+				strcpy(nomeUtente, getString());
+
+				if (ricercaUtente(radUtente, nomeUtente) != 1)
+					printf("\nIl suo nome utente non e' presente tra gli utenti registrati!\n");
+				else
+				{
+					while (1)
+					{
+						printf("\nPassw: ");
+						strcpy(passw, getString());
+						if (controlloPassw(radUtente, nomeUtente, passw) == 1)
+							break;
 						else
-							printf("Ok! Ora inserisca una password:\n");
-					} while (flag != 0);
-					scanf("%s", passwd);
-					printf("Perfetto! Registrazione completata. Sta per essere reindirizzato alla schermata principale...\n");
+							printf("\nPassword errata! Riprovi!\n");
+					}
 
-					break;
-				case '0':
-					printf("\nChiusura in corso...\n");
-					quit = 1;
-					break;
-				default:
-					printf("\nRichiesta non valida\n");
-					break;
-			}
+					if (strcmp(nomeUtente, "_System") == 0)
+						menùAdmin();
+					else
+						menùUtente();
+				}
+
+				break;
+			case '2':
+				do 
+				{
+					printf("\nSalve, per registrarsi inserisca un username: ");
+					strcpy(nomeUtente, getString());
+					flag = ricercaUtente(radUtente, nomeUtente);
+					if (flag == 1)
+						printf("Nome utente non disponibile. Riprovi:\n");
+					else
+						printf("\nOk! Ora inserisca una password: ");
+				} while (flag != 0);
+				strcpy(passw, getString());
+				printf("\nPerfetto! Registrazione completata. Sta per essere reindirizzato alla schermata principale...\n");
+
+				break;
+			case '0':
+				printf("\nChiusura in corso...\n");
+				quit = 1;
+				break;
+			default:
+				printf("\nRichiesta non valida\n");
+				break;
+		}
 	} while (quit != 1);
 
 	return 0;
 }
-
