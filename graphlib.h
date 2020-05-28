a0c4fc281723855bb3df569ec19251df1f2a4894
oresteLeone <64261072+oresteLeone@users.noreply.github.com>
2020-05-28 20:08:04 +0200

    aggiunte funzioni grafo da modificare

--- graphlib.h
+++ graphlib.h
@@ -8,4 +8,26 @@ typedef struct graphType {
     edge** adj;     //lista di adiacenza
 } Graph;
 
+//inizializzazione grafo
+Graph* initGraph(int n);
+
+//Free del grafo
+void freeGraph(Graph* G);
+
+//stampa del grafo
+void printGraph(Graph* G);
+
+//aggiungi nodo al grafo
+void addNode(Graph* G);
+
+//rimozione di un nodo dal grafo
+void removeNode(Graph* G, int node_to_remove);
+
+//aggiunge arco da source a target
+void addEdge(Graph* G, int source, int target);
+
+//rimuove arco da source a target
+edge* removeEdge(Graph* G, int source, int target);
+
+
 #endif
